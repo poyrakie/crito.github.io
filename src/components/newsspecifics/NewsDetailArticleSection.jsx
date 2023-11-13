a12c04d0bebe6f582d47cb54bffb5e0adfe07ed0
poyrakie <johan.astrom91@live.se>
2023-11-13 12:33:58 +0100

    carousel up and running, with styling completed aswell

--- src/components/newsspecifics/NewsDetailArticleSection.jsx
+++ src/components/newsspecifics/NewsDetailArticleSection.jsx
@@ -2,13 +2,37 @@ import React from 'react'
 import { useArticle } from '../../context/ArticleContext'
 import { Link } from 'react-router-dom'
 
+
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
+
 const NewsDetailArticleSection = () => {
     const { articles } = useArticle()
 
     const getMonth = (e) => {
         const month = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
         return month[e-1]
-      }
+    }
+
+    const responsive = {
+    superLargeDesktop: {
+        // the naming can be any, depends on you.
+        breakpoint: { max: 4000, min: 3000 },
+        items: 5
+    },
+    desktop: {
+        breakpoint: { max: 3000, min: 1024 },
+        items: 3
+    },
+    tablet: {
+        breakpoint: { max: 1024, min: 464 },
+        items: 2
+    },
+    mobile: {
+        breakpoint: { max: 464, min: 0 },
+        items: 1
+    }
+    };
   return (
     <section className="news-detail-article-section">
 
@@ -20,20 +44,28 @@ const NewsDetailArticleSection = () => {
             </div>
 
             <div className="article-box">
-                {
-                    articles.map(article => (
-                        <Link to={`/news/${article.id}`} className="article-item" key={article.id}> 
-                            <img src={article.imageUrl} />
-                            <div className="absolute-date">
-                                <h3>{article.published.slice(8, 10)}</h3>
-                                <p>{getMonth(article.published.slice(5,7))}</p>
-                            </div>
-                            <p>{article.category}</p>
-                            <h3>{article.author}</h3>
-                            <p>{article.content}</p>
-                        </Link>
-                    ))
-                }
+                <Carousel responsive={responsive}
+                showDots={true}
+                infinite={true}
+                dotListClass="custom-dot-list-style"
+                renderDotsOutside={true}
+                >
+                    {
+                        articles.map(article => (
+                            <Link to={`/news/${article.id}`} className="article-item" key={article.id}> 
+                                <img src={article.imageUrl} />
+                                <div className="absolute-date">
+                                    <h3>{article.published.slice(8, 10)}</h3>
+                                    <p>{getMonth(article.published.slice(5,7))}</p>
+                                </div>
+                                <p>{article.category}</p>
+                                <h3>{article.author}</h3>
+                                <p>{article.content}</p>
+                            </Link>
+                        ))
+                    }
+                </Carousel>
+
             </div>
         </div>
     </section>
